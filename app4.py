98b3a20ef73fb29915653a8a97dfcbe4cb310cff
nhiyen23 <takenbyyn2356@gmail.com>
2026-02-07 17:36:20 +0700

    Update app4.py

--- app4.py
+++ app4.py
@@ -63,7 +63,7 @@ st.markdown("""
     
     .nav-bar a {
         font-family: 'Montserrat';
-        font-weight: 600;
+        font-weight: 500;
         color: #2B1E4B;
         text-decoration: none;
         margin: 0 1.5rem;
