dacf6cac0df5d732e3d458c69bcd7bd8cd2c2b93
nhiyen23 <takenbyyn2356@gmail.com>
2026-02-07 22:56:48 +0700

    Update app4.py

--- app4.py
+++ app4.py
@@ -26,7 +26,7 @@ st.markdown("""
     }
     
     .block-container {
-        padding: 0rem 4% !important;
+        padding: 0rem 3% !important;
         max-width: 90% !important;
         margin-left: 30px !important;
         margin-right: 30px !important;
